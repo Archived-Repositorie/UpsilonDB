9f6c6527cba9d950eac407ca5217718f1e05965e
JustFoxxo <73485099+JustFoxx@users.noreply.github.com>
2022-12-30 11:09:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# java-template-app
+
+Free to use under CC0 license template for your next java app! It comes with out of box github packages support and full gradle setup.
---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    public static Logger logger = LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args) {
+        logger.info("Hello World!");
+    }
+}
